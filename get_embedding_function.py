c42319a326bc1b078f3427244cc7bd9b762d631e
Eden Mengste <edenmengste@gmail.com>
2025-08-18 00:02:23 +0400

    Update get_embedding_function.py

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -1,7 +1,8 @@
-from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings.bedrock import BedrockEmbeddings
+
 
 def get_embedding_function():
-    embeddings = OpenAIEmbeddings(
-        model="text-embedding-3-small"
+    embeddings = BedrockEmbeddings(
+        credentials_profile_name="default", region_name="us-east-1"
     )
     return embeddings
